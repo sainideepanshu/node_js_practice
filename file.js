f89b6c0f61c4bdc459dba242acf28a5f9dbc676b
deepanshu <deepanshukamlesh@gamil.com>
2024-05-05 00:02:24 +0530

    file system in node.js

---
+++ file.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+
+// Synchronous , it will return something
+fs.writeFileSync("./test.txt","Hey we have created a file Synchronously");  // writeFileSync overrides the given file 
+
+
+// Asynchronously , they expect a callback function
+fs.writeFile("./dummy.txt","Hey we have created a file Asynchronously",(err) => {    // writeFile overrides the given file 
+
+});
+
+
+// Synchronous , it will return something
+const result = fs.readFileSync("./contacts.txt","utf-8");
+
+console.log(result);
+
+// Asynchronously , they expect a callback function
+fs.readFile("./contacts.txt","utf-8",(err,result) => {
+
+    if(err){
+        console.lof("Error",err);
+    }
+    else{
+        console.log(result);
+    }
+});
+
+// Synchronous 
+fs.appendFileSync("./test.txt",new Date().getDate().toLocaleString());   // It will not override the given file but append the content provided
+
+
+//fs.cpSync("./test.txt","./copy.txt");  // it will copy the content of text.txt to copy.txt
+
+//fs.unlinkSync("./copy.txt");  // it will delete the file
+
+console.log(fs.statSync("./test.txt"));
+
+fs.mkdirSync("my_docs");
+
+fs.mkdirSync("my_docs/a/b",{recursive:true});
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const math = require("./math.js");  // this require function is from node.js
 
 
-// agar hum require function k andar sirf ye likhe require("fs") , to wo fs module ko node ki directory m search karega
+// agar hum require function k andar sirf ye likhe require("fs") , to wo fs module ko node ki directory m search karega , fs is the built in module in node.js
 // agar hum require function k andar sirf ye likhe require("./fs") , to wo fs module ko current working directory m search karega
 
 
