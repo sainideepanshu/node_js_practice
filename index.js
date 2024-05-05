44b67472d3faed74fe2d2f0449cfcea6029bd717
deepanshu <deepanshukamlesh@gamil.com>
2024-05-05 15:17:23 +0530

    Building HTTP server in node.js

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const math = require("./math.js");  // this require function is from node.js
 console.log("Hello Deepu");
 
 console.log(math.add(2,5));
+
+const http = require("http");
+
+const fs = require("fs");
+
+const myServer = http.createServer((req,res) => {
+
+    const log = `${Date.now()} : ${req.url} New Request recieved \n`;
+
+    fs.appendFile("log.txt",log,(err,data)=>{
+
+        res.end("Hello world from Server");
+    });
+});
+
+myServer.listen(8000, ()=>{
+
+    console.log("Server started on PORT");
+
+});
