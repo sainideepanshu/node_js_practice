f43ca56910b3d88bbbe53c21243af2d99974f9e1
deepanshu <deepanshukamlesh@gamil.com>
2024-05-05 18:45:21 +0530

    handling URLs in node.js

--- index.js
+++ index.js
@@ -14,13 +14,37 @@ const http = require("http");
 
 const fs = require("fs");
 
+const url = require("url");  // pehle ye url ko node modules or dependencies m search karega agar usme nahi to bhi ye node js k built in modules m search karega
+
 const myServer = http.createServer((req,res) => {
+
+    if(req.url === "/favicon.ico"){
+        return res.end();
+    }
 
     const log = `${Date.now()} : ${req.url} New Request recieved \n`;
 
+    const myUrl = url.parse(req.url,true);
+
+    console.log(myUrl);
+
     fs.appendFile("log.txt",log,(err,data)=>{
 
-        res.end("Hello world from Server");
+        switch(myUrl.pathname){
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":    //http://localhost:8000/about?myName=Deepanshu&useId=1
+                const username = myUrl.query.myName;
+                res.end(`I am ${username}`);
+                break;
+            case "/search":   //http://localhost:8000/search?search_query=javascript+tic+tac+toe
+                const search = myUrl.query.search_query;
+                res.end("Here are your results for "+search);
+                break;
+            default:
+                res.end("404 not found");
+        }
     });
 });
 
